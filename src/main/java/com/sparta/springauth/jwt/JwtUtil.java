0881d1dce10e530ad58cf347301ecc3606545f01
jdm2131 <jdm2131@naver.com>
2024-07-08 19:27:36 +0900

    [feat] 회원가입

--- src/main/java/com/sparta/springauth/jwt/JwtUtil.java
+++ src/main/java/com/sparta/springauth/jwt/JwtUtil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,10 +21,9 @@ import java.util.Base64;
 import java.util.Date;
 
 @Component
+@Slf4j
 public class JwtUtil {
     // JWT 데이터
-
-    // Header KEY 값
     public static final String AUTHORIZATION_HEADER = "Authorization";
     // 사용자 권한 값의 KEY
     public static final String AUTHORIZATION_KEY = "auth";
@@ -37,9 +37,6 @@ public class JwtUtil {
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 
-    // 로그 설정
-    public static final Logger log = LoggerFactory.getLogger("JWT 관련 로그");
-
     @PostConstruct
     public void init() {
         byte[] bytes = Base64.getDecoder().decode(secretKey);
@@ -49,28 +46,27 @@ public class JwtUtil {
     // JWT 생성
     public String createToken(String username, UserRoleEnum role) {
         Date date = new Date();
-
         return BEARER_PREFIX +
                 Jwts.builder()
                         .setSubject(username) // 사용자 식별자값(ID)
-                        .claim(AUTHORIZATION_KEY, role) // 사용자 권한
-                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
+                        .claim(AUTHORIZATION_KEY,role) // 사용자 권한
+                        .setExpiration(new Date(date.getTime())) // 만료 시간
                         .setIssuedAt(date) // 발급일
                         .signWith(key, signatureAlgorithm) // 암호화 알고리즘
                         .compact();
     }
+
     // JWT Cookie 에 저장
     public void addJwtToCookie(String token, HttpServletResponse response) {
         token = URLEncoder.encode(token, StandardCharsets.UTF_8)
                 .replaceAll("\\+", "%20"); // Cookie Value 에는 공백이 불가능해서 encoding 진행
 
-        Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token); // Name-Value
+        Cookie cookie = new Cookie(AUTHORIZATION_HEADER, token);
         cookie.setPath("/");
-
-        // Response 객체에 Cookie 추가
         response.addCookie(cookie);
     }
-    // JWT 토큰 substring
+
+    // JWT Token Substring
     public String substringToken(String tokenValue) {
         if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
             return tokenValue.substring(7);
@@ -78,12 +74,13 @@ public class JwtUtil {
         log.error("Not Found Token");
         throw new NullPointerException("Not Found Token");
     }
+
     // JWT 검증
     public boolean validateToken(String token) {
-        try {
+        try{
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return true;
-        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+        }catch (SecurityException | MalformedJwtException | SignatureException e) {
             log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
         } catch (ExpiredJwtException e) {
             log.error("Expired JWT token, 만료된 JWT token 입니다.");
@@ -94,6 +91,7 @@ public class JwtUtil {
         }
         return false;
     }
+
     // JWT에서 사용자 정보 가져오기
     public Claims getUserInfoFromToken(String token) {
         return Jwts.parserBuilder().setSigningKey(key).build()
---
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -0,0 +1,47 @@
+package com.sparta.springauth.service;
+
+import com.sparta.springauth.dto.SignupRequestDto;
+import com.sparta.springauth.entity.User;
+import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
+
+    public void signup(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        Optional<User> checkUsername = userRepository.findByUsername(username);
+        if (checkUsername.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다");
+        }
+        String email = requestDto.getEmail();
+        Optional<User> checkEmail = userRepository.findByEmail(email);
+        if (checkEmail.isPresent()) {
+            throw new IllegalArgumentException("중복된 이메일이 존재합니다");
+        }
+
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (requestDto.isAdmin()) {
+            if (!ADMIN_TOKEN.equals(requestDto.getAdminToken())) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+}
