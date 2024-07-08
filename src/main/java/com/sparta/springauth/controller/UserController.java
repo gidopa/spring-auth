178d91faba02f5e0c73896630226d4f37e018b32
jdm2131 <jdm2131@naver.com>
2024-07-08 19:52:43 +0900

    [feat] Jwt Login

--- src/main/java/com/sparta/springauth/controller/UserController.java
+++ src/main/java/com/sparta/springauth/controller/UserController.java
@@ -1,14 +1,18 @@
 package com.sparta.springauth.controller;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -29,6 +33,19 @@ public class UserController {
     @PostMapping("/user/signup")
     public String signup(@ModelAttribute SignupRequestDto requestDto) {
         userService.signup(requestDto);
+        log.info("User 회원가입 , Id : {}, password : {}", requestDto.getUsername(), requestDto.getPassword());
         return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("/user/login")
+    public String login(@ModelAttribute LoginRequestDto requestDto, HttpServletResponse response) {
+        try {
+            userService.login(requestDto, response);
+        } catch (Exception e) {
+            log.info("Login error");
+            return "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/sparta/springauth/service/UserService.java
+++ src/main/java/com/sparta/springauth/service/UserService.java
@@ -1,10 +1,14 @@
 package com.sparta.springauth.service;
 
+import com.sparta.springauth.dto.LoginRequestDto;
 import com.sparta.springauth.dto.SignupRequestDto;
 import com.sparta.springauth.entity.User;
 import com.sparta.springauth.entity.UserRoleEnum;
+import com.sparta.springauth.jwt.JwtUtil;
 import com.sparta.springauth.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -12,10 +16,12 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     private final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
 
@@ -43,5 +49,29 @@ public class UserService {
 
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse response) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> {
+                    log.info("login user not found , username : {}", requestDto.getUsername());
+                    return new IllegalArgumentException("등록된 사용자가 없습니다");
+                });
+
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            log.info("login password not match , username : {}", requestDto.getUsername());
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 response 에 넘겨줌
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, response);
+
+
     }
 }
